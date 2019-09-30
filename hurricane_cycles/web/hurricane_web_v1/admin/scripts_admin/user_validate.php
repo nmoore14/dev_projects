982874efd2eebf8f5201853252fd62ea3401702a
Nick Moore <nickmoore_1@live.com>
2019-09-30 19:28:57 -0400

    Setting up validate and config files

---
+++ hurricane_cycles/web/hurricane_web_v1/admin/scripts_admin/user_validate.php
@@ -0,0 +1,11 @@
+<?php
+  /**
+   * Validate users to login and CRUD for the user database
+   * Created by: Nick Moore
+   * September 30, 2019
+   */
+?>
+
+function user_login(username, password) {
+  
+}
