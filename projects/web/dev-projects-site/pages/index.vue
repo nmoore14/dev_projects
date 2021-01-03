2f7e3000d81fba785c73af686268a217aa7947dd
Nick Moore <nickmoore_1@live.com>
2021-01-03 00:01:20 -0500

    Updating dependencies and adding tailwind config file

--- projects/web/dev-projects-site/pages/index.vue
+++ projects/web/dev-projects-site/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="main-home" class="flex flex-col flex-nowrap min-h-screen justify-center bg-red-200">
-    <div id="hero-home" class="flex flex-col flex-nowrap w-11/12 h-64 mx-auto bg-gray-200">
+  <div id="main-home" class="flex flex-col justify-center min-h-screen bg-red-200 flex-nowrap">
+    <div id="hero-home" class="flex flex-col w-11/12 mx-auto bg-gray-200 flex-nowrap h-120">
     </div>
     <div class="flex flex-col w-5/6 min-h-screen mx-auto bg-blue-200">
     </div>
---
+++ projects/web/dev-projects-site/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    height: {
+      '100': '25rem',
+      '120': '30rem'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
