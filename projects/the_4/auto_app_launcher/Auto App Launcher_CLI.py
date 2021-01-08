9a5bed3f9deefb3d9c9d743ac2e4b3d2674cbb68
69oo69 <53282201+69oo69@users.noreply.github.com>
2021-01-08 18:11:06 -0500

    spacing

--- projects/the_4/auto_app_launcher/Auto App Launcher_CLI.py
+++ projects/the_4/auto_app_launcher/Auto App Launcher_CLI.py
@@ -1,12 +1,10 @@
 import os
 
-
 running = True
 
 print("(1)Add applications:", "\n(2)Launch applications:", "\n(3) Exit")
 user_input = input("Select one of the following: ")
 apps = []# populate this list with exe files
-
 
 while running:
     user_input
